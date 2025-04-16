d36cd85882b2c814934a439e0bbc57fc227f21c4
Sibi Aathithyaa <165630783+SibiAathithyas@users.noreply.github.com>
2025-04-16 14:25:40 +0530

    Delete _app.js

--- _app.js
+++
@@ -1,9 +0,0 @@
-// pages/_app.js
-import '../styles/globals.css';  // Import global styles
-import React from 'react';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
